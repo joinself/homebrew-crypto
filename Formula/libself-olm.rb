e5c8ad3885fec048a9a11497dca49ee166e0962e
Tom Bevan <tom.bevan@r3labs.io>
2020-06-08 19:07:41 +0100

    use correct strategy

--- Formula/libself-olm.rb
+++ Formula/libself-olm.rb
@@ -3,7 +3,7 @@ require_relative "../custom_download_strategy.rb"
 class LibselfOlm < Formula
   desc "Self fork of olm"
   homepage "https://selfid.net"
-  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.15.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.15.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
   sha256 "2c9c4ead02621d353eeb7754e5d708a1fcc15d1c1843310a2e6e6591994aa4e0"
 
   head do
