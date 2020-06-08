110c04e2af723cf788f56f2045e38b602ab6ad2b
Tom Bevan <tom.bevan@r3labs.io>
2020-06-08 14:34:12 +0100

    allow pulling from private release

--- Formula/libself-olm.rb
+++ Formula/libself-olm.rb
@@ -1,7 +1,7 @@
 class LibselfOlm < Formula
   desc "Self fork of olm"
   homepage "https://selfid.net"
-  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.15.tar.gz"
+  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.15.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "2c9c4ead02621d353eeb7754e5d708a1fcc15d1c1843310a2e6e6591994aa4e0"
 
   head do
