8c6ccc48e3c189b611831fb2af2a1cfc0d94b52a
Tom Bevan <tom.bevan@r3labs.io>
2020-06-08 21:31:25 +0100

    fix checksum

--- Formula/libself-olm.rb
+++ Formula/libself-olm.rb
@@ -4,7 +4,7 @@ class LibselfOlm < Formula
   desc "Self fork of olm"
   homepage "https://selfid.net"
   url "https://github.com/aldgate-ventures/self-olm/archive/0.1.16.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
-  sha256 "2c9c4ead02621d353eeb7754e5d708a1fcc15d1c1843310a2e6e6591994aa4e0"
+  sha256 "49d08d07ef33ef89c4baf5abd40e386614c5dca9f3b1c44d8af3528381dbbf31"
 
   depends_on "libsodium"
   depends_on "cmake" => :build
