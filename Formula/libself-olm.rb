764abb198d7b1c3e9d24ec13d4ef85d8346b936a
Tom Bevan <tom.bevan@r3labs.io>
2020-06-08 20:29:14 +0100

    bump olm version

--- Formula/libself-olm.rb
+++ Formula/libself-olm.rb
@@ -3,11 +3,11 @@ require_relative "../custom_download_strategy.rb"
 class LibselfOlm < Formula
   desc "Self fork of olm"
   homepage "https://selfid.net"
-  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.15.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.16.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
   sha256 "2c9c4ead02621d353eeb7754e5d708a1fcc15d1c1843310a2e6e6591994aa4e0"
 
+  depends_on "libsodium"
   depends_on "cmake" => :build
-  depends_on "libsodium" => :build
 
   def install
     system "cmake", ".", "-Bbuild", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
