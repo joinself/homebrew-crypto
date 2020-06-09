4c5caa88f38cf49de5a08096284d54ff6bc0e5b3
Tom Bevan <tom.bevan@r3labs.io>
2020-06-09 18:14:46 +0100

    fix omemo url

--- Formula/libself-omemo.rb
+++ Formula/libself-omemo.rb
@@ -3,7 +3,7 @@ require_relative "../custom_download_strategy.rb"
 class LibselfOmemo < Formula
   desc "Omemo for selfs implementation of olm"
   homepage "https://selfid.net"
-  url "https://github.com/aldgate-ventures/self-omemo/archive/0.1.17.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/aldgate-ventures/self-omemo/archive/0.1.2.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
   sha256 "78326c487dd5afcabe170d09b2e116a3cac43f008836f3b3ea9cfc3657985740"
 
   depends_on "libsodium"
