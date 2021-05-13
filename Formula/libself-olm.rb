9b92b6baf56f85baaeddabccceb37b756a660361
tom <tom.bevan@aldgate-ventures.com>
2021-05-13 03:25:43 -0700

    fix brew style offences

--- Formula/libself-olm.rb
+++ Formula/libself-olm.rb
@@ -1,17 +1,16 @@
 # Copyright 2020 Self Group Ltd. All Rights Reserved.
 
-require_relative "../lib/custom_download_strategy.rb"
+require_relative "../lib/custom_download_strategy"
 
 class LibselfOlm < Formula
   desc "Self fork of olm"
   homepage "https://selfid.net"
-  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.17.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/aldgate-ventures/self-olm/archive/0.1.17.tar.gz", using: CustomGitHubPrivateRepositoryDownloadStrategy
   sha256 "90b7bef7dab491cefe3f7a76d52c2ceb4c22df5535641bb3da1aaa0ec74b5b77"
 
   bottle do
     root_url "https://dl.bintray.com/selfid/bottles-crypto"
-    cellar :any
-    sha256 "3d5c926cb7eccc312c9d6c93ebad275b08c396c4f4fc1c9da84378d86b3f5cc1" => :catalina
+    sha256 cellar: :any, catalina: "3d5c926cb7eccc312c9d6c93ebad275b08c396c4f4fc1c9da84378d86b3f5cc1"
   end
 
   depends_on "cmake" => :build
--- Formula/libself-omemo.rb
+++ Formula/libself-omemo.rb
@@ -1,17 +1,16 @@
 # Copyright 2020 Self Group Ltd. All Rights Reserved.
 
-require_relative "../lib/custom_download_strategy.rb"
+require_relative "../lib/custom_download_strategy"
 
 class LibselfOmemo < Formula
   desc "Omemo for selfs implementation of olm"
   homepage "https://selfid.net"
-  url "https://github.com/aldgate-ventures/self-omemo/archive/0.1.2.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/aldgate-ventures/self-omemo/archive/0.1.2.tar.gz", using: CustomGitHubPrivateRepositoryDownloadStrategy
   sha256 "78326c487dd5afcabe170d09b2e116a3cac43f008836f3b3ea9cfc3657985740"
 
   bottle do
     root_url "https://dl.bintray.com/selfid/bottles-crypto"
-    cellar :any
-    sha256 "985206fb61b6e9006027eba76bc30d120c16099caf0f56ee8ce26f41c32575bb" => :catalina
+    sha256 cellar: :any, catalina: "985206fb61b6e9006027eba76bc30d120c16099caf0f56ee8ce26f41c32575bb"
   end
 
   depends_on "rust" => :build
